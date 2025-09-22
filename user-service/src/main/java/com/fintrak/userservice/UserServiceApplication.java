cf78972baa12842f6af1d9604f9c514f7fd4dd07
Mick <mklagassey@gmail.com>
2025-09-22 19:43:37 -0400

    add more logging to debug user service

--- user-service/src/main/java/com/fintrak/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/fintrak/userservice/UserServiceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
 
 @SpringBootApplication
 public class UserServiceApplication {
@@ -19,11 +21,22 @@ public class UserServiceApplication {
 
     // Add this bean to log the test secret on startup
     @Bean
-    CommandLineRunner commandLineRunner(@Value("${fintrack.test.secret:SECRET_NOT_FOUND}") String testSecret) {
+    @Profile("prod") // Ensure this only runs in the 'prod' profile
+    CommandLineRunner commandLineRunner(@Value("${debug.retrieved.password:SECRET_NOT_FOUND}") String retrievedPassword) {
         return args -> {
-            logger.info(">>>>>>>>>>>>> FINTRAK TEST SECRET <<<<<<<<<<<<<");
-            logger.info("Successfully fetched test secret: {}", testSecret);
-            logger.info(">>>>>>>>>>>>> FINTRAK TEST SECRET <<<<<<<<<<<<<");
+            logger.info("====================== DEBUGGING SECRET ======================");
+            logger.info("Attempting to use password retrieved from Secret Manager: [{}]", retrievedPassword);
+            logger.info("==============================================================");
         };
     }
+
+    // Add this bean to log the test secret on startup
+//    @Bean
+//    CommandLineRunner commandLineRunner(@Value("${fintrack.test.secret:SECRET_NOT_FOUND}") String testSecret) {
+//        return args -> {
+//            logger.info(">>>>>>>>>>>>> FINTRAK TEST SECRET <<<<<<<<<<<<<");
+//            logger.info("Successfully fetched test secret: {}", testSecret);
+//            logger.info(">>>>>>>>>>>>> FINTRAK TEST SECRET <<<<<<<<<<<<<");
+//        };
+//    }
 }
